f1e07767c41731ae5dd41a302a807a8bdd264f2f
sjh <sjh990503@163.com>
2023-07-30 14:06:35 +0800

    feat: add icon and update config

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # lazy-css-snippet
 
-实在记不住QAQ
+目前仅支持`scss`变量
+
+## example
+
+{
+  '9px': '$font-xxxs;', 
+  '10px': '$font-xxs;',
+  '11px': '$font-xs;',
+  '12px': '$font-s;',
+  '13px': '$font-m;',
+  '14px': '$font-xm;',
+  '16px': '$font-l;',
+  '18px': '$font-xl;',
+  '20px': '$font-xxl;',
+  '24px': '$font-xxxl',
+  '#1181d7': '$color-primary;',
+  '#eda12d': '$color-yellow;',
+  '#fdbb56': '$color-yellow1;',
+  ...
+}
+
+以后可能会改为读取全部配置，不限文件类型的模式 (逃
 
 ## License
 
--- src/completion.ts
+++ src/completion.ts
@@ -2,18 +2,27 @@ import type { CompletionItemProvider, ExtensionContext } from 'vscode'
 import { CompletionItem, CompletionItemKind, languages } from 'vscode'
 import { SnippetMap } from './meta'
 
-const LANGUAGES = ['vue', 'css', 'scss', 'less', 'html', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact']
+const LANGUAGES = ['vue', 'scss']
+const REG = /^#+/
 
 export function RegisterCompletion(ctx: ExtensionContext) {
   const provider: CompletionItemProvider = {
     provideCompletionItems() {
 
       const completionItems = Object.keys(SnippetMap).map((key: string) => {
-        const completionItem = new CompletionItem(key)
-        completionItem.kind = CompletionItemKind.Snippet
-        completionItem.insertText = `font-size: ${SnippetMap[key]}`
-        completionItem.detail = `font-size: ${SnippetMap[key]}`
-        return completionItem
+        if (REG.test(key)) {
+          const completionItem = new CompletionItem(key)
+          completionItem.kind = CompletionItemKind.Snippet
+          completionItem.insertText = `color: ${SnippetMap[key]}`
+          completionItem.detail = `color: ${key}`
+          return completionItem
+        } else {
+          const completionItem = new CompletionItem(key)
+          completionItem.kind = CompletionItemKind.Snippet
+          completionItem.insertText = `font-size: ${SnippetMap[key]}`
+          completionItem.detail = `font-size: ${key}`
+          return completionItem
+        }
       })
       return completionItems
     }
